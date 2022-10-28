12054609c150f2cf226e2a62eccb7cf02f0eaa30
Niky <nick.vusko@gmail.com>
2022-10-28 12:49:27 +0200

    edit 22/10/28

---
+++ Matrix_gen.py
@@ -0,0 +1,103 @@
+# Author:  NIKOLA LADISLAVOVA, nikola.ladislavova@gmail.com
+# Created: Feb 25, 2021
+# Purpose: Chromatography
+
+# Copyright (c) 2021, Nikola Ladislavova, UCT, Prague
+
+import pandas as pd
+import numpy as np
+
+
+# FUNCTIONS
+
+def get_column_names(start_table_area):
+    list_of_renames = []
+    list_of_compounds = []
+    for compound in start_table_area.index:
+        list_of_compounds.append(compound)
+    count = 0
+    for element in list_of_compounds:
+        for ilimint in range(count,len(list_of_compounds)):
+            rename = list_of_compounds[count]+"/"+list_of_compounds[ilimint]
+            list_of_renames.append(rename)  
+        count = count+1
+    return list_of_renames
+
+
+def get_matrix_height(start_table_height, list_of_renames):
+    final_table_height = pd.DataFrame()
+    for column in start_table_height.iteritems():
+        column_name = column[0]
+        column_x = column[1]
+        array = np.column_stack(column_x)
+        array2 = array
+        with np.errstate(divide="ignore", invalid="ignore"):
+            result = array.reshape(-1, 1)/array2
+            df_matrix = pd.DataFrame(result)
+            df_matrix = df_matrix.fillna("0")
+            df_matrix.to_csv("results/height/height_matrix_"+column_name, sep="\t")
+            df_matrix = df_matrix.where(np.triu(np.ones(df_matrix.shape), k=0).astype(np.bool))
+            df_matrix = pd.DataFrame(df_matrix.stack().to_frame().values).T
+            df_matrixx = df_matrix.rename(index={0: column_name})
+            final_table_height = final_table_height.append(df_matrixx)
+    final_table_height.set_axis([list_of_renames], axis=1, inplace=True)
+    final_table_height.to_csv("results/height/height_singlerow.txt", sep="\t")
+
+
+def get_matrix_area(start_table_area, list_of_renames):
+    final_table_area = pd.DataFrame()
+    for column in start_table_area.iteritems():
+        column_name = column[0]
+        column_x = column[1]
+        array = np.column_stack(column_x)
+        array2 = array
+        with np.errstate(divide = "ignore", invalid = "ignore"):
+            result = array.reshape(-1, 1)/array2
+            df_matrix = pd.DataFrame(result)
+            df_matrix = df_matrix.fillna("0")
+            df_matrix.to_csv("results/area/area_matrix_"+column_name, sep = "\t")
+            df_matrix = df_matrix.where(np.triu(np.ones(df_matrix.shape), k=0).astype(np.bool))
+            df_matrix = pd.DataFrame(df_matrix.stack().to_frame().values).T
+            df_matrixx = df_matrix.rename(index={0: column_name})
+            final_table_area = final_table_area.append(df_matrixx)
+    final_table_area.set_axis([list_of_renames], axis=1, inplace=True)
+    final_table_area.to_csv("results/area/area_singlerow.txt", sep="\t")
+
+
+def main():
+    print("Set up environment")
+    #########################
+    # get input from the user#
+    a = ((input("Enter the name of file for area matrix: ")) + ".txt")
+    h = ((input("Enter the name of file for height matrix: ")) + ".txt")
+    if a != "" and h != "":
+        start_table_height = pd.read_csv(h, sep="\t", header=0, index_col=0)
+        start_table_area = pd.read_csv(a, sep="\t", header=0, index_col=0)
+
+        print("Getting column names...")
+        list_of_renames = get_column_names(start_table_area)
+
+        print("Creating matrices for areas and heights")
+        get_matrix_height(start_table_height, list_of_renames)
+        get_matrix_area(start_table_area, list_of_renames)
+    elif a != "" and h == "":
+        start_table_area = pd.read_csv(a, sep="\t", header=0, index_col=0)
+
+        print("Getting column names...")
+        list_of_renames = get_column_names(start_table_area)
+
+        print("Creating matrix for areas only")
+        get_matrix_area(start_table_area, list_of_renames)
+    elif a == "" and h != "":
+        start_table_height = pd.read_csv(a, sep="\t", header=0, index_col=0)
+
+        print("Getting column names...")
+        list_of_renames = get_column_names(start_table_height)
+
+        print("Creating matrix for heights only")
+        get_matrix_height(start_table_height, list_of_renames)
+    else:
+        print("No valid arguments for generating ration matrices were given.")
+
+
+main()
--- README.md
+++ README.md
@@ -1,1 +1,44 @@
 # 2D_data_chrom_automation
+
+This is a small package of helper functions for automation of data preparation steps during handling of chromatographic data.
+
+### Data Inputs: ### 
+
+The input data should contain columns named: "Name","Area", and "Height".
+The default separator is "\t"
+
+### Sort_by_name script
+
+Sort_by_name script takes all .txt files in the folder and generates a summary table.
+
+Rows correspond to detected chemical compounds (every unique name has its own row - the comparison is based on strings).
+
+Each column represents a sample which has been stored in the processed folder.
+
+** note that the current version is only taking in chemical compounds which start with the "MX" prefix. The behaviour can be edited on line # 34 of the code.
+
+### Matrix_gen
+
+Matrix_gen takes the summary tables (rows = compounds, columns = samples) as an input and generates:
+1) ratio matrix for each sample
+2) summary one row representation of all upper triangle of the ratio matrix transformed into the single line representation.
+
+The single line representation can be used as an input to commercial software programs as the first column is the name of the sample,
+and the rest of the columns represents the ratios - variables.
+
+### Stat_dev
+A simple function for cleaning up the data. If there are multiple rows in the input file, the script recalculates deviation for each variable,
+and sorts them and the resulting table contains only the top X (this parameter is also chosen by the user at the beginning of the script) variables with the least variation. 
+The script also eliminates variables (columns) with sum of the zero values higher than the threshold that the user sets at the start of the script.
+
+** note that the current version of the script is designed for work with the ratio matrices. It replaces all values 1 for value 0.
+this behaviour can be changed at line #18, respectively #27.
+
+## Merging rations
+
+This simple script takes two types of input tables - for definition of variables of interest and tables for analysis.
+All variables involved in "define" tables will be used for construction of the final table. Only defined variables for the previous step will be chosen from the variables in tables set for the analysis.
+
+The final table is the summary of all samples, but the variables only correspond to those from the "define" set of tables.
+The script also eliminates variables (columns) with sum of the zero values higher than the threshold that the user sets at the start of the script.
+This elimination is executed on the columns of the final summary table.
